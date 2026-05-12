4853b9905ab1067bf0c4198d26e449502052a507
VannyWi <lokiwilson2002@gmail.com>
2026-05-11 21:17:54 -0500

    Mensaje hola y adios

--- src/main/java/com/example/todos/controller/TodoController.java
+++ src/main/java/com/example/todos/controller/TodoController.java
@@ -40,6 +40,7 @@ public class TodoController {
     @PostMapping
     public ResponseEntity<TodoResponse> create(@Valid @RequestBody TodoCreateRequest request) {
         TodoResponse created = todoService.create(request);
+        System.out.println("adios");
         return ResponseEntity.created(URI.create("/api/todos/" + created.id())).body(created);
     }
 
